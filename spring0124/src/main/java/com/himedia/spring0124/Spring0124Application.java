6f2acf51eb2b5c6626737084a5bfda1d24652454
DESKTOP-DBJQEKM\USER <khkh0923@naver.com>
2025-01-24 10:24:47 +0900

    프로젝트 시작

---
+++ spring0124/src/main/java/com/himedia/spring0124/Spring0124Application.java
@@ -0,0 +1,13 @@
+package com.himedia.spring0124;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring0124Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring0124Application.class, args);
+    }
+
+}
---
+++ spring0124/src/test/java/com/himedia/spring0124/Spring0124ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himedia.spring0124;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring0124ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
